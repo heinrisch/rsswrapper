0f58b81f81f7f81b04481e9aa7c02ed8aef74b6b
Henrik Sandström <henrik@wrapp.com>
2013-05-08 20:24:42 +0200

    Fix column update

--- rssw/http_handler.go
+++ rssw/http_handler.go
@@ -66,14 +66,14 @@ func WriteToDatabase() {
 		fmt.Println(err)
 		return
 	}
-	updatestmt, err2 := tx.Prepare("update news set item=(?), trend=(?) where title=(?) and source=(?)")
+	updatestmt, err2 := tx.Prepare("update news set item=?, trend=? where title=? and source=?")
 	if err2 != nil {
 		fmt.Println(err2)
 		return
 	}
 
-	defer updatestmt.Close()
 	defer stmt.Close()
+	defer updatestmt.Close()
 	for _, item := range items {
 		jsonBlob, _ := json.Marshal(item)
 		_, err = stmt.Exec(item.Title, item.Source, item.UnixTime(), item.TwitterStats.Count+item.FacebookStats.Shares, jsonBlob)
